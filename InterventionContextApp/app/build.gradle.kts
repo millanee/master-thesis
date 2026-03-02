5bc8ecbbbe44f912b0a0702daa00d6a100488a84
millanee <kgx953@alumni.ku.dk>
2026-03-02 13:23:57 +0400

    build: add dependencies for geofencing and viewmodel compose

--- InterventionContextApp/app/build.gradle.kts
+++ InterventionContextApp/app/build.gradle.kts
@@ -47,6 +47,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+    implementation(libs.play.services.location)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
