3a51641b02219ff057daa56811e9910a5f70c9ea
millanee <kgx953@alumni.ku.dk>
2026-03-28 15:20:51 +0100

    feat(location context): set time limits to 15 minutes and set standard study schedule

--- InterventionContextApp/app/src/main/java/com/millane/thesis/application/study/SessionManager.kt
+++ InterventionContextApp/app/src/main/java/com/millane/thesis/application/study/SessionManager.kt
@@ -76,10 +76,10 @@ class SessionManager(
     private var designFrictionLaunchInProgress: Boolean = false
 
     private val mainHandler = Handler(Looper.getMainLooper())
-    private val goalAdvancementDelayMs = 1 * 60 * 1000L
+    private val goalAdvancementDelayMs = 15 * 60 * 1000L
     private var goalAdvancementRunnable: Runnable? = null
 
-    private val selfTrackingDelayMs = 1 * 60 * 1000L
+    private val selfTrackingDelayMs = 15 * 60 * 1000L
     private var selfTrackingRunnable: Runnable? = null
 
     /**
